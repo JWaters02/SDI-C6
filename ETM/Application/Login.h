22b2687c9e351b6b5c2ca3d0bcc655fb6260de68
N0916167 <N0916167@my.ntu.ac.uk>
2022-01-08 08:21:21 +0000

    Refactored classes

---
+++ ETM/Application/Login.h
@@ -0,0 +1,14 @@
+//
+// Created by bobby on 08/01/2022.
+//
+
+#ifndef ETM_LOGIN_H
+#define ETM_LOGIN_H
+
+
+class Login {
+
+};
+
+
+#endif //ETM_LOGIN_H
---
+++ ETM/Database/DBHandler.h
@@ -0,0 +1,14 @@
+//
+// Created by bobby on 08/01/2022.
+//
+
+#ifndef ETM_DBHANDLER_H
+#define ETM_DBHANDLER_H
+
+
+class DBHandler {
+
+};
+
+
+#endif //ETM_DBHANDLER_H
